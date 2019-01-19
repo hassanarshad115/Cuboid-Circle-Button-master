bc5cdcaf4b5ae948728c7a6f98c71cc8d02dea82
Hassan <hassanarshad115@gmail.com>
2019-01-19 20:28:38 +0500

    Initial commit

---
+++ app/src/main/java/com/cuboid/circlebutton/CircleButtonActivity.java
@@ -0,0 +1,17 @@
+package com.cuboid.circlebutton;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cuboid.cuboidcirclebutton.CuboidButton;
+
+
+public class CircleButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activtiy_design);
+    }
+}
